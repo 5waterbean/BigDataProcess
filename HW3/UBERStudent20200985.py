f381a07abfd2446db04baab393d5fb774ce512a6
5waterbean <waterbean0563@naver.com>
2022-10-26 23:40:14 +0900

    add hw3 code

--- HW3/UBERStudent20200985.py
+++ HW3/UBERStudent20200985.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+command = sys.argv
+inputfile = command[1]
+outputfile = command[2]
+
+with open(inputfile, "rt") as ipf:
+	for line in ipf:
+		text = line.split(",")
