955930b4190541515e39078604c25a20f3defd5f
5waterbean <waterbean0563@naver.com>
2022-10-27 14:51:29 +0900

    add hw3 code

--- HW3/UBERStudent20200985.py
+++ HW3/UBERStudent20200985.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 import sys
+import calendar
 
 command = sys.argv
 inputfile = command[1]
 outputfile = command[2]
+dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
+list = []
 with open(inputfile, "rt") as ipf:
 	for line in ipf:
 		text = line.split(",")
+		bnumber = text[0]
+		date = text[1].split("/")
+		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+		vehicles = text[2]
+		trips = text[3][:-1]
+		list.append(bnumber + "," + dayofweek[day] + " " + vehicles + "," + trips + "\n")
+
+with open(outputfile, "at") as opf:
+	for line in list:
+		opf.write(line)
