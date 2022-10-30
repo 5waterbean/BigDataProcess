fb96359dc42f6202ca1943c202b297ec43e19f38
5waterbean <waterbean0563@naver.com>
2022-10-30 17:41:45 +0900

    add hw3 code

--- HW3/UBERStudent20200985.py
+++ HW3/UBERStudent20200985.py
@@ -7,17 +7,23 @@ inputfile = command[1]
 outputfile = command[2]
 dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
-list = []
+dic = dict()
 with open(inputfile, "rt") as ipf:
 	for line in ipf:
 		text = line.split(",")
 		bnumber = text[0]
 		date = text[1].split("/")
 		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
-		vehicles = text[2]
-		trips = text[3][:-1]
-		list.append(bnumber + "," + dayofweek[day] + " " + vehicles + "," + trips + "\n")
+		vehicles = int(text[2])
+		trips = int(text[3][:-1])
+		key = bnumber + "," + dayofweek[day]
+		if key in dic:
+			value = dic[key].split(",")
+			vehicles += int(value[0])
+			trips += int(value[1])
+		dic[key] = str(vehicles) + "," + str(trips)
 
 with open(outputfile, "at") as opf:
-	for line in list:
-		opf.write(line)
+	items = dic.items()
+	for item in items:
+		opf.write(item[0] + " " + str(item[1]) + "\n")
